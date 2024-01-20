36794ab7779dec1e45da942fb1cef124fc4d990a
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-01-20 15:20:28 +0800

    feat(frontend): add landing page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,32 +4,44 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import ExplorePage from "./components/pages/Explore";
 import HomePage from "./components/pages/Home";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LandingPage from "./components/pages/Landing";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="My Groups"
+        component={HomePage}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="group" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExplorePage}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="explore" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="My Groups"
-          component={HomePage}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesome name="group" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Explore"
-          component={ExplorePage}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcons name="explore" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingPage} />
+        <Stack.Screen name="Main" component={TabNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ frontend/components/pages/Landing.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function LandingPage() {
+  return <View>Landing page</View>;
+}
