132a20281de40baefa8dee4e6cf76a92973dfcfc
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-01-20 20:48:54 +0800

    feat(backend): add query and body to log

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  console.log(`[${req.method} ${req.path}]`);
+  console.log(`[${req.method} ${req.path}] query=${req.query} body=`, req.body);
   next();
 });
 
