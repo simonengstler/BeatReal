1250be12517f60d74b9c96ea76db6dc155def7c0
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-01-20 19:12:58 +0800

    build: update encrypt/decrypt script

--- frontend/decrypt.sh
+++ frontend/decrypt.sh
@@ -6,8 +6,12 @@ encrypted_file="private.tar.gpg"
 # Define the name of the decrypted file
 decrypted_file="private.tar"
 
-echo "Enter password to decrypt file:"
-read -s tar_password
-echo $tar_password | gpg --batch --quiet --yes --passphrase-fd 0 -d $encrypted_file > $decrypted_file
+tar_password=$1
+if [ -z "$tar_password" ]; then
+    echo "Enter password for zip file:"
+    read -s tar_password
+fi
+
+echo $tar_password | gpg --batch --quiet --yes --passphrase-fd 0 -d $encrypted_file >$decrypted_file
 
 tar -xf $decrypted_file
--- frontend/encrypt.sh
+++ frontend/encrypt.sh
@@ -9,6 +9,10 @@ directory="./private"
 # Use the zip command to zip all files in the specified directories
 tar -cf $tarfile $directory
 
-echo "Enter password for zip file:"
-read -s tar_password
+tar_password=$1
+if [ -z "$tar_password" ]; then
+    echo "Enter password for zip file:"
+    read -s tar_password
+fi
+
 echo $tar_password | gpg --batch --yes --passphrase-fd 0 -c $tarfile
