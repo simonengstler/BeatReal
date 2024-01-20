7b04e2a2a083864401fa625dc710eeef7503e4a8
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-01-20 20:22:22 +0800

    feat(backend): add request middleware

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const usersRouter = require('./routes/users');
-const groupsRouter = require('./routes/groups');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const usersRouter = require("./routes/users");
+const groupsRouter = require("./routes/groups");
 
 const app = express();
-var cors = require('cors')
 const PORT = 3000;
 
-app.use(cors())
-
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`[${req.method} ${req.path}]`);
+  next();
+});
 
-app.use('/api/users', usersRouter.router);
-app.use('/api', groupsRouter);
+app.use("/api/users", usersRouter.router);
+app.use("/api", groupsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
