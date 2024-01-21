a0cec4d2338532fa5d823eaa60db8b05ecc974c4
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-01-21 11:43:45 +0800

    feat(frontend): add online manager to handle online status

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,21 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NetInfo from "@react-native-community/netinfo";
+import {
+  QueryClient,
+  QueryClientProvider,
+  onlineManager,
+} from "@tanstack/react-query";
 import React from "react";
+import { MenuProvider } from "react-native-popup-menu";
 import { AuthProvider } from "./components/context/AuthContext";
 import { RootNavigator } from "./components/routes/RootNavigator";
-import { MenuProvider } from "react-native-popup-menu";
 
 const queryClient = new QueryClient();
+
+onlineManager.setEventListener((setOnline) => {
+  return NetInfo.addEventListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+});
 
 export default function App() {
   return (
